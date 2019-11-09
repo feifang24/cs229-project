e365f8d8d4c308f99de246e357300e5957d75edd
feifang24 <ffang19@stanford.edu>
2019-11-09 21:27:10 +0000

    changed flags

--- sample_run.sh
+++ sample_run.sh
@@ -13,8 +13,8 @@ python3 bert/run_classifier.py \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
   --max_seq_length=128 \
-  --train_batch_size=16 \
+  --train_batch_size=8 \
   --learning_rate=2e-5 \
   --num_train_epochs=20.0 \
   --output_dir=$OUTPUT_DIR \
-  --save_checkpoints_steps=6
+  --save_checkpoints_steps=30
