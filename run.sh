69972fa04594e31fe0ace4b684f4df692e1a5b5f
feifang24 <ffang19@stanford.edu>
2019-11-11 06:28:03 +0000

    renamed run bash script

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
+export SUBSET_DIR=sd100
 export MODEL_ID=992534b7   # this is ignored if do_train=True
 
 export BERT_BASE_DIR=gs://cs229-checkpoints/uncased_L-12_H-768_A-12
 export TASK_NAME=imdb
 export DATA_DIR=gs://cs229-data/imdb-data
-export SUBSET_DIR=sd100
 export OUTPUT_DIR=gs://cs229-checkpoints/$TASK_NAME
 
 python3 bert/run_classifier.py \
   --task_name=$TASK_NAME \
-  --do_train=false \
+  --do_train=true \
   --do_eval=true \
   --data_dir=$DATA_DIR \
   --subset_dir=$SUBSET_DIR \
