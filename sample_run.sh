115d38f16e6111337519f04c840eeb1c78728cae
feifang24 <ffang19@stanford.edu>
2019-11-10 21:58:34 -0800

    implemented model directory management

--- sample_run.sh
+++ sample_run.sh
@@ -1,13 +1,14 @@
 export BERT_BASE_DIR=gs://cs229-checkpoints/uncased_L-12_H-768_A-12
-export IMDB_DIR=gs://cs229-data/imdb-data
+export TASK_NAME=imdb
+export DATA_DIR=gs://cs229-data/imdb-data
 export SUBSET_DIR=sd100
-export OUTPUT_DIR=gs://cs229-checkpoints/sample-run
+export OUTPUT_DIR=gs://cs229-checkpoints/$TASK_NAME
 
 python3 bert/run_classifier.py \
-  --task_name=imdb \
+  --task_name=$TASK_NAME \
   --do_train=true \
   --do_eval=true \
-  --data_dir=$IMDB_DIR \
+  --data_dir=$DATA_DIR \
   --subset_dir=$SUBSET_DIR \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
