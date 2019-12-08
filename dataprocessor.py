1bcfc0719a9d9e8b82af1f500ed8d21269254432
feifang24 <ffang19@stanford.edu>
2019-12-08 02:41:57 -0800

    implemented export pipeline

--- dataprocessor.py
+++ dataprocessor.py
@@ -48,10 +48,26 @@ class ImdbProcessor():
           guid="unused_id", text_a=text, text_b=None, label=label))
     return examples
 
+  def _create_examples_from_csv(input_file):
+    """Reads a comma separated value file."""
+    examples = []
+    with tf.gfile.Open(input_file, "r") as f:
+      reader = csv.reader(f)
+      for line in reader:
+        text, label = line
+        text = text.strip().replace("<br />", " ")
+        str_label = 'pos' if label == 1 else 'neg'
+        examples.append(InputExample(
+          guid="unused_id", text_a=text, text_b=None, label=label))
+    return examples
+
   def _get_train_dev_examples(self, dataset):
     assert dataset in self.allDataSets
     if self.trainDevExamples is None:
       # load examples if they haven't been loaded before
-      self.trainDevExamples = self._create_examples(os.path.join(self.dataDirPath, dataset))
+      if dataset.startswith('wd'):
+        self.trainDevExamples = _create_examples_from_csv(os.path.join(self.dataDirPath, "{}.csv".format(dataset)))
+      else:
+        self.trainDevExamples = self._create_examples(os.path.join(self.dataDirPath, dataset))
     return train_test_split(self.trainDevExamples, 
                         train_size=self.trainDevSplit[dataset], random_state=42)
