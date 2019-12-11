e2673126d07fb94602d5c26847e9eb0bb7df9962
feifang24 <ffang19@stanford.edu>
2019-12-11 06:01:27 +0000

    debugged

--- bert/run_regression.py
+++ bert/run_regression.py
@@ -344,7 +344,7 @@ def convert_single_example(ex_index, example, label_list, max_seq_length,
     tf.logging.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
     tf.logging.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
     tf.logging.info("segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
-    tf.logging.info("label: %s (id = %d)" % (example.label, label_id))
+    tf.logging.info("label: {} (id = {})".format(example.label, label_id))
 
   feature = InputFeatures(
       input_ids=input_ids,
@@ -716,8 +716,6 @@ def main(_):
 
   task_name = FLAGS.task_name.lower()
 
-  if task_name not in processors:
-    raise ValueError("Task not found: %s" % (task_name))
 
   processor = RegressionProcessor(FLAGS.data_dir) #processors[task_name](FLAGS.data_dir)
 
